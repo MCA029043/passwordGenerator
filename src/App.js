e9e2832627ec60b375adc2750ed7b0300ac40b32
MCA029043 <ajaypotlia99@gmail.com>
2023-11-10 23:17:17 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [password, setPassword] = useState('');
+  const [length, setLength] = useState(8);
+  const [includeUppercase, setIncludeUppercase] = useState(true);
+  const [includeNumbers, setIncludeNumbers] = useState(true);
+  const [includeSymbols, setIncludeSymbols] = useState(true);
+
+  const generatePassword = () => {
+    const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+    let characters = lowercaseLetters;
+    if (includeUppercase) characters += uppercaseLetters;
+    if (includeNumbers) characters += numbers;
+    if (includeSymbols) characters += symbols;
+
+    let generatedPassword = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      generatedPassword += characters[randomIndex];
+    }
+
+    setPassword(generatedPassword);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(password);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Password Generator</h1>
+
+      <div>
+        <label>Enter length of the password: </label>
+        <input
+          type="number"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label>Include Uppercase: </label>
+        <input
+          type="checkbox"
+          checked={includeUppercase}
+          onChange={() => setIncludeUppercase(!includeUppercase)}
+        />
+      </div>
+
+      <div>
+        <label>Include Numbers: </label>
+        <input
+          type="checkbox"
+          checked={includeNumbers}
+          onChange={() => setIncludeNumbers(!includeNumbers)}
+        />
+      </div>
+
+      <div>
+        <label>Include Symbols: </label>
+        <input
+          type="checkbox"
+          checked={includeSymbols}
+          onChange={() => setIncludeSymbols(!includeSymbols)}
+        />
+      </div>
+
+      <button onClick={generatePassword}>Generate Password</button>
+
+      <div>
+        <h2>Generated Password:</h2>
+        <input type="text" value={password} readOnly />
+        <button onClick={copyToClipboard}>Copy password</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
